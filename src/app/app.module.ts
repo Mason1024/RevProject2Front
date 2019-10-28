a82ac81fcfe467471b9ef7c5f8913585b0af9e2f
Jcoffin96 <jasoncoffin59@yahoo.com>
2019-10-28 13:18:51 -0400

    Added Login Component/ redid the user model

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -8,13 +8,69 @@ export class User {
 	phone_number:string;
 	postings:Posting[]; // Array of post Ids
 
-	constructor(u_id:number, username:string, password:string, email:string, phone_number:string, postings:Posting[]) {
-		this.u_id = u_id;
-		this.username = username;
-		this.password = password;
-		this.email = email;
-		this.phone_number = phone_number;
-		this.postings = postings;
+	constructor(){}
+
+	public setAll(newUser:User):void{
+			this.$u_id = newUser.$u_id;
+			this.$username = newUser.$username;
+			this.$password = newUser.$password;
+			this.$email = newUser.$email;
+			this.$phone_number = newUser.$phone_number;
+			this.$postings = newUser.$postings;
 	}
 
+	 //userID
+	 public get $u_id(): number {
+		return this.u_id;
+	}
+
+	public set $u_id(value: number) {
+		this.u_id = value;
+	}
+
+    //username
+	public get $username(): string {
+		return this.username;
+	}
+
+	public set $username(value: string) {
+		this.username = value;
+	}
+
+    //password
+	public get $password(): string {
+		return this.password;
+	}
+
+	public set $password(value: string) {
+		this.password = value;
+	}
+
+    //email
+	public get $email(): string {
+		return this.email;
+	}
+
+	public set $email(value: string) {
+		this.email = value;
+    }
+
+	//phone number
+	public get $phone_number(): string {
+		return this.phone_number;
+	}
+
+	public set $phone_number(value: string) {
+		this.phone_number = value;
+	}
+	
+	
+    //Postings
+	public get $postings(): Posting[] {
+		return this.postings;
+	}
+
+	public set $postings(value: Posting[]) {
+		this.postings = value;
+    }
 }
