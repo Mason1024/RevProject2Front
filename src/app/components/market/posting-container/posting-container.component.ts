c41c18b1a289e52e1197c450e93389265cf799c6
Dillon Mason <d14mason@yahoo.com>
2019-10-28 15:10:24 -0400

    seed components

---
+++ src/app/components/market/posting-container/posting-container.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-posting-container',
+  templateUrl: './posting-container.component.html',
+  styleUrls: ['./posting-container.component.css']
+})
+export class PostingContainerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
