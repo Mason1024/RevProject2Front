f268294557d15932812f74a6f453e963776c8a2d
Jaredsk74 <jsk3@pct.edu>
2019-10-29 14:23:59 -0400

    Login base
    Minor change to header as well

--- src/app/components/header/header/header.component.ts
+++ src/app/components/header/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/models/user';
+import { CurrentUserService } from 'src/app/services/current-user.service';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +9,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  user:User;
 
-  ngOnInit() {
+  constructor(private uService:CurrentUserService) { }
+
+  ngOnInit() { // May need to switch to obversable method
+    this.user = this.uService.getStaticUser();
+  }
+
+  hub(user:User):void {
+      // Route to user-hub
+  }
+
+  logout(user:User):void {
+    user = null;
+    // Route to home
+  }
+
+  login():void {
+    // Route to login
+  }
+
+  register():void {
+    // Route to new-user
   }
 
 }
