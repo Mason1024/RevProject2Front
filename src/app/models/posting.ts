231b3d6b63b756efda75a8d686b561afe24f1e2d
Jcoffin96 <jasoncoffin59@yahoo.com>
2019-10-30 15:21:33 -0400

    Put title into posting model

--- src/app/models/posting.ts
+++ src/app/models/posting.ts
@@ -4,16 +4,18 @@ export class Posting{
 
     id:number;
     user:User;
+    title:string;
     description:string;
     category:string;
     location:string;
     initDate:number;
     endDate:number;
 
-    constructor(id:number,user:User,description:string,category:string,
+    constructor(id:number,user:User,description:string,title:string,category:string,
         location:string,initDate:number,endDate:number){
             this.id = id;
             this.user = user;
+            this.title = title;
             this.description = description;
             this.category = category;
             this.location = location;
