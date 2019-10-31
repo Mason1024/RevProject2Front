4b068ce54ccd96072167796f3f5e858a7efcd033
aaronmccoy <ammccoy.professional@gmail.com>
2019-10-31 15:47:48 -0400

    update to posting-form, probably needs review

--- src/app/components/userHub/posting-form/posting-form.component.ts
+++ src/app/components/userHub/posting-form/posting-form.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { CategoryService } from 'src/app/services/category.service';
+import { PostingService } from 'src/app/services/posting.service';
+import { CurrentUserService } from 'src/app/services/current-user.service';
+import { User } from 'src/app/models/user';
+import { Posting } from 'src/app/models/posting';
 
 @Component({
   selector: 'app-posting-form',
@@ -8,10 +12,34 @@ import { CategoryService } from 'src/app/services/category.service';
 })
 export class PostingFormComponent implements OnInit {
 
-  constructor(private CategoryService:CategoryService ) { }
+  user:User = null;
+
+  constructor(private categoryService:CategoryService, 
+    private postingService:PostingService, 
+    private userService:CurrentUserService ) { }
 
   ngOnInit() {
+    for(let i = 0; i < this.categoryService.getAllCategories().length; i++)
+    document.getElementById("categoriesDropdown").innerHTML +=
+    "<option id='" + this.categoryService.getAllCategories()[i] + "'>" + 
+    this.categoryService.getAllCategories()[i] + "</option>";
+
+    this.userService.currentUser$.subscribe(data =>{ 
+      this.user = data
+    });
   }
+
+  createPost(){
+   let post:Posting = new Posting(0, this.user.id, 
+    document.getElementById("description").value, 
+    document.getElementById("categoriesDropdown").value, 
+    document.getElementById("location").value,
+    0,document.getElementById("endDate"), 
+    document.getElementById("title").value, 
+    "");
+    this.postingService.createPosting(post);
+  }
+
 
 }
 
