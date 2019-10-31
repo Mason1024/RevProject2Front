8d7440490ec5c8cd10c51daa4971df49e8fe1f97
Mason1024 <d14mason@yahoo.com>
2019-10-30 21:37:18 -0400

    Routing for most of the nav buttons

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MarketContainerComponent } from './components/market/market-container/market-container.component';
 import { UserHubComponent } from './components/userHub/user-hub/user-hub.component';
+import { LoginComponent } from './components/header/login/login.component';
+import { NewUserComponent } from './components/header/new-user/new-user.component';
 
 
 const routes: Routes = [
   {path:"market",component:MarketContainerComponent},
   {path:"userHub",component:UserHubComponent},
+  {path:"login",component:LoginComponent},
+  {path:"newUser",component:NewUserComponent},
   {path:"**",component:MarketContainerComponent}
 ];
 
