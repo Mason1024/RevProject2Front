9290f4a52b1ba6a9aa47f30f07ff66c7f25b4f4a
Jaredsk74 <jsk3@pct.edu>
2019-11-03 17:46:29 -0500

    Basic Service tests

--- src/app/services/user.service.spec.ts
+++ src/app/services/user.service.spec.ts
@@ -6,6 +6,7 @@ import { User } from '../models/user';
 import { fail } from 'assert';
 
 describe('UserService', () => {
+  let user:User;
   beforeEach(() => TestBed.configureTestingModule({
     imports: [ HttpClientModule ]
   })
@@ -16,13 +17,43 @@ describe('UserService', () => {
     const service: UserService = TestBed.get(UserService);
     expect(service).toBeTruthy();
   });
+  
+  it ("#createUser should create a user and receive the user back", () => {
+    const service: UserService = TestBed.get(UserService);
+    user = new User(0, "TestUsername", "TestPassword", "TestEmail", "8005551234");
+    service.createUser(user).then(info => {
+      user = info;
+      expect(user.id !== 0);
+    }).catch (Response => {
+      fail("User not created");
+    });
+  })
 
   it("#getUserById should return a user object", () => {
     const service: UserService = TestBed.get(UserService);
-    service.getUserById(332).then(info => {
+    service.getUserById(user.id).then(info => {
       expect(info).toBeDefined();
     }).catch(response => {
       fail("Promise threw an error");
+    });
+  })
+
+  it("#updateUser should update the user and return the new data", () => {
+    const service: UserService = TestBed.get(UserService);
+    user.username = "UpdatedUsename";
+    service.updateUser(user).then(info => {
+      user = info;
+      expect(user.username !== "TestUsername")
+    })
+  })
+
+  it("#deleteUser should remove the test user", () => {
+    const service: UserService = TestBed.get(UserService);
+    service.deleteUser(user.id);
+    service.getUserById(user.id).then(info => {
+      fail("Promise shouldn't have succeeded")
+    }).catch(response => {
+      expect(response);
     });
   })
 });
